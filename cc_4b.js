6e8293d60405c7d665d3371e2bafa4221d93a018
Federico Montejo <fmontejo@usf.edu>
2025-09-17 19:42:59 -0400

    Created my objects

---
+++ cc_4b.js
@@ -0,0 +1,8 @@
+// creating my objects
+let products = [
+    {sku:"0123", name:"Phone", category:"Electronics",price:899.99,inventory:20},
+    {sku:"0124", name:"Soccer Ball", category:"Sports",price:129.99,inventory:45},
+    {sku:"0125", name:"Washing Machine", category:"Electronics",price:1299.99,inventory:15},
+    {sku:"0126", name:"Pokemon Cards", category:"Toys",price:19.99,inventory:60},
+    {sku:"0127", name:"T-Shirt", category:"Clothing",price:15.99,inventory:80},
+];
